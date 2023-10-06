fc44643b955a2c844ae4cc8bf330c98ae234f717
Priyanka Choudhary <priyanka.choudhary@publicissapient.com>
2023-10-06 16:35:42 +0530

    kfp pipeline

--- kubeflow-pipeline/pipeline.py
+++ kubeflow-pipeline/pipeline.py
@@ -1,12 +1,56 @@
 from typing import List
 from kfp import dsl, compiler
-from kfp.dsl import component, Input, Output, Metrics, Model
+from kfp.dsl import component, Input, Output, Metrics, Model, Artifact, Dataset
 
-# from google.cloud import aiplatform
+from google.cloud import aiplatform
 
 
-@component(packages_to_install=["scikit-learn", "xgboost", "pandas", "joblib"])
-def train_model_op(model_name: str, model: Output[Model], accuracy: Output[Metrics]):
+@component(
+    base_image="python:3.9",
+    packages_to_install=["google-cloud-bigquery[pandas]==3.10.0"],
+)
+def fetch_bigquery_dataset(
+    project_id: str,
+    dataset_id: str,
+    view_name: str,
+    dataset: Output[Dataset],
+):
+    """Exports from BigQuery to a CSV file.
+
+    Args:
+        project_id: The Project ID.
+        dataset_id: The BigQuery Dataset ID. Must be pre-created in the project.
+        view_name: The BigQuery view name.
+
+    Returns:
+        dataset: The Dataset artifact with exported CSV file.
+    """
+    from google.cloud import bigquery
+
+    client = bigquery.Client(project=project_id)
+
+    table_name = f"{project_id}.{dataset_id}.{view_name}"
+    query = """
+    SELECT
+      *
+    FROM
+      `{table_name}`
+    """.format(
+        table_name=table_name
+    )
+
+    job_config = bigquery.QueryJobConfig()
+    query_job = client.query(query=query, job_config=job_config)
+    df = query_job.result().to_dataframe()
+    df.to_csv(dataset.path, index=False)
+
+
+@component(
+    base_image="python:3.9",
+    packages_to_install=["scikit-learn==1.0.2", "pandas==1.3.5", "joblib==1.1.0"],
+)
+def train_model_op(model: Output[Model], accuracy: Output[Metrics]):
+    import os
     import pandas as pd
     from sklearn.model_selection import train_test_split
     import joblib
@@ -14,72 +58,42 @@ def train_model_op(model_name: str, model: Output[Model], accuracy: Output[Metri
     data = pd.read_csv(
         "https://raw.githubusercontent.com/Priyanka4pc/ml-academy/main/preprocessed.csv"
     )
-    
+
     X = data.drop(["Price"], axis=1)
     Y = data["Price"]
 
     train_X, test_X, train_Y, test_Y = train_test_split(X, Y, random_state=0)
 
-    if model_name == "LinearRegression":
-        from sklearn.linear_model import LinearRegression
+    from sklearn.ensemble import RandomForestRegressor
 
-        model = LinearRegression()
-    elif model_name == "RandomForestRegressor":
-        from sklearn.ensemble import RandomForestRegressor
+    rfmodel = RandomForestRegressor(max_leaf_nodes=100, random_state=1)
+    rfmodel.fit(train_X, train_Y)
+    os.makedirs(model.path, exist_ok=True)
+    joblib.dump(rfmodel, os.path.join(model.path, "model.joblib"))
 
-        model = RandomForestRegressor(max_leaf_nodes=100, random_state=1)
-    elif model_name == "XGBRegressor":
-        from xgboost import XGBRegressor
-
-        model = XGBRegressor()
-
-    model.fit(train_X, train_Y)
-    model_path = f"{model_name}.joblib"
-    joblib.dump(model, model_path)
-    model.uri = model_path
-
-    accuracy = model.score(test_X, test_Y)
+    score = rfmodel.score(test_X, test_Y)
+    accuracy.log_metric("accuracy", score)
 
 
-@component()
-def select_best_model_op(models: list, accuracies: list, best_model: Output[Model]):
-    # Find the index of the model with the highest accuracy
-    best_index = accuracies.index(max(accuracies))
+@component(base_image="python:3.9", packages_to_install=["google-cloud-aiplatform"])
+def deploy_model_op(
+    model: Input[Model],
+    vertex_endpoint: Output[Artifact],
+    vertex_model: Output[Model],
+):
+    from google.cloud import aiplatform
 
-    # Select the best model
-    best_model.uri = models[best_index].uri
+    aiplatform.init(project="gcp-tutorial-400612")
 
+    deployed_model = aiplatform.Model.upload(
+        display_name="rf-model",
+        artifact_uri=model.uri,
+        serving_container_image_uri="us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-2:latest",
+    )
+    endpoint = deployed_model.deploy(machine_type="n1-standard-4")
 
-# def deploy_model_op(model: Input[Artifact]):
-#     # Initialize the Vertex AI Python SDK client
-#     client = aiplatform.gapic.ModelServiceClient()
-
-#     # Specify the project and location
-#     project = 'my-project' # replace with your project ID
-#     location = 'us-central1' # replace with your location
-
-#     # Specify the model and endpoint details
-#     model_display_name = 'my-model' # replace with your model display name
-#     endpoint_display_name = 'my-endpoint' # replace with your endpoint display name
-
-#     # Upload the model
-#     model = client.upload_model(
-#         parent=f'projects/{project}/locations/{location}',
-#         model=aiplatform.gapic.Model(display_name=model_display_name, artifact_uri=model.uri)
-#     )
-
-#     # Create the endpoint
-#     endpoint = client.create_endpoint(
-#         parent=f'projects/{project}/locations/{location}',
-#         endpoint=aiplatform.gapic.Endpoint(display_name=endpoint_display_name)
-#     )
-
-#     # Deploy the model to the endpoint
-#     client.deploy_model(
-#         endpoint=endpoint.name,
-#         deployed_model=aiplatform.gapic.DeployedModel(model=model.name),
-#         traffic_split={'0': 100}
-#     )
+    vertex_endpoint.uri = endpoint.resource_name
+    vertex_model.uri = deployed_model.resource_name
 
 
 @dsl.pipeline(
@@ -87,40 +101,20 @@ def select_best_model_op(models: list, accuracies: list, best_model: Output[Mode
     description="A pipeline that trains multiple models in parallel and selects best one to deploy",
 )
 def train_multiple_models_pipeline():
-    model_names = [
-        "LinearRegression",
-        "RandomForestRegressor",
-        "XGBRegressor",
-    ]  # replace with your model names
-    # model_names = [{"model_name": "LinearRegression"}, {"model_name": "RandomForestRegressor"}, {"model_name": "XGBRegressor"}]
-
-    # models = []
-    # accuracies = []
-    # with dsl.ParallelFor(model_names) as model_name:
-    #     train_op = train_model_op(model_name=model_name)
-        # models.append(train_op.outputs['model'])
-        # accuracies.append(train_op.outputs['accuracy'])
-    
-
-    # best_model_op = select_best_model_op(
-        # models=dsl.Collected(train_op.outputs["model"]),
-        # accuracies=dsl.Collected(train_op.outputs["accuracy"]),
-    # )
-
-    models = []
-    model_scores = []
-
-    for model_name in model_names:
-        train_op = train_model_op(model_name=model_name)
-        models.append(train_op.outputs['model'])
-        model_scores.append(train_op.outputs['accuracy'])
-
-    select_best_model_op(models=models, accuracies=model_scores)
-
-    # deploy_model_op(best_model_op.outputs['best_model'])
+    train_op = train_model_op()
+    deploy_model_op(model=train_op.outputs["model"])
 
 
 if __name__ == "__main__":
-    compiler.Compiler().compile(
-        train_multiple_models_pipeline, "train_multiple_models_pipeline.yaml"
+    pipeline_file_name = "train_and_deploy_pipeline.yaml"
+    compiler.Compiler().compile(train_multiple_models_pipeline, pipeline_file_name)
+
+    pipeline_job = aiplatform.PipelineJob(
+        display_name=f"test",
+        template_path=pipeline_file_name,
+        enable_caching=False,
+    )
+
+    response = pipeline_job.submit(
+        service_account="gcp-tutorial@gcp-tutorial-400612.iam.gserviceaccount.com"
     )
