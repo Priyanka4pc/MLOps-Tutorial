8ad359b62f71c651af5bbecce8ddec53d9092f92
Priyanka Choudhary <priyanka.choudhary@publicissapient.com>
2023-10-11 04:35:09 +0530

    update infra

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,28 +1,3 @@
-provider "google" {
-  alias = "tokengen"
-}
-# get config of the client that runs
-data "google_client_config" "default" {
-  provider = google.tokengen
-
-}
-data "google_service_account_access_token" "sa" {
-  provider               = google.tokengen
-  target_service_account = "mlops-tutorial@mlops-project-401617.iam.gserviceaccount.com"
-  lifetime               = "600s"
-  scopes = [
-    "https://www.googleapis.com/auth/cloud-platform",
-  ]
-}
-
-/******************************************
-  GA Provider configuration
- *****************************************/
-provider "google" {
-  access_token = data.google_service_account_access_token.sa.access_token
-  project      = var.project_id
-}
-
 # define terraform provider
 terraform {
   backend "gcs" {
@@ -31,10 +6,9 @@ terraform {
   }
 }
 
-# provider "google" {
-#   project = var.project_id
-#   # credentials = file(var.creds_file)  # required when terraform authenticates on it's own
-#   impersonate_service_account = "mlops-tutorial@mlops-project-401617.iam.gserviceaccount.com"
-#   region = var.region
-#   zone   = var.zone
-# }
+provider "google" {
+  project = var.project_id
+  # credentials = file(var.creds_file)  # required when terraform authenticates on it's own
+  region = var.region
+  zone   = var.zone
+}
