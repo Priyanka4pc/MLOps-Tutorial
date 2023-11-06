3678965e5c13785968a89b28a851af77b2beb564
Priyanka Choudhary <priyanka.choudhary@publicissapient.com>
2023-11-06 09:33:41 +0530

    add model comparison step

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -62,6 +62,7 @@ def fetch_features(
     packages_to_install=["scikit-learn==1.0.2", "pandas==1.3.5", "joblib==1.1.0"],
 )
 def train_model_op(
+    model_name: str,
     target_column: str,
     dataset: Input[Dataset],
     model: Output[Model],
@@ -79,16 +80,36 @@ def train_model_op(
     Y = data[target_column]
 
     train_X, test_X, train_Y, test_Y = train_test_split(X, Y, random_state=0)
+    if model_name == "DecisionTreeRegressor":
+        from sklearn.tree import DecisionTreeRegressor
 
-    from sklearn.ensemble import RandomForestRegressor
+        regmodel = DecisionTreeRegressor(max_depth=5, random_state=1)
 
-    rfmodel = RandomForestRegressor(max_leaf_nodes=100, random_state=1)
-    rfmodel.fit(train_X, train_Y)
+    elif model_name == "RandomForestRegressor":
+        from sklearn.ensemble import RandomForestRegressor
+
+        regmodel = RandomForestRegressor(max_leaf_nodes=100, random_state=1)
+
+    regmodel.fit(train_X, train_Y)
     os.makedirs(model.path, exist_ok=True)
-    joblib.dump(rfmodel, os.path.join(model.path, "model.joblib"))
+    joblib.dump(regmodel, os.path.join(model.path, f"{model_name}.joblib"))
 
-    score = rfmodel.score(test_X, test_Y)
-    accuracy.log_metric("Random Forest Accuracy", score)
+    score = regmodel.score(test_X, test_Y)
+    accuracy.log_metric("Accuracy", score)
+
+
+@component(base_image="python:3.10")
+def best_model(
+    train1_accuracy: Input[Metrics],
+    train2_accuracy: Input[Metrics],
+    train1_model: Input[Model],
+    train2_model: Input[Model],
+    best_model: Output[Model],
+):
+    if train1_accuracy.metadata["Accuracy"] > train2_accuracy.metadata["Accuracy"]:
+        best_model.uri = train1_model.uri
+    else:
+        best_model.uri = train2_model.uri
 
 
 @component(base_image="python:3.10", packages_to_install=["google-cloud-aiplatform"])
@@ -130,7 +151,7 @@ def train_and_deploy_pipeline(
     entity_id_column: str = "index_column",
     time_column: str = "time",
     target_column: str = "price",
-    model_name: str = "rf-model",
+    model_name: str = "sklearn-model",
     machine_type: str = "n1-standard-2",
     serving_container_image_uri: str = "us-docker.pkg.dev/vertex-ai/prediction/sklearn-cpu.1-2:latest",
 ):
@@ -144,15 +165,32 @@ def train_and_deploy_pipeline(
         entity_id_column=entity_id_column,
         time_column=time_column,
     )
-    train_op = train_model_op(
-        target_column=target_column, dataset=fetch_data_op.outputs["dataset"]
+    with dsl.ParallelFor(
+        models=["RandomForestRegressor", "DecisionTreeRegressor"], parallelism=1
+    ) as model_name:
+        train1 = train_model_op(
+            model_name=model_name,
+            target_column=target_column,
+            dataset=fetch_data_op.outputs["dataset"],
+        )
+        train2 = train_model_op(
+            model_name=model_name,
+            target_column=target_column,
+            dataset=fetch_data_op.outputs["dataset"],
+        )
+
+    best_model_op = best_model(
+        train1.outputs["accuracy"],
+        train1.outputs["model"],
+        train2.outputs["accuracy"],
+        train2.outputs["model"],
     )
     deploy_model_op(
         project_id=project_id,
         model_name=model_name,
         machine_type=machine_type,
         serving_container_image_uri=serving_container_image_uri,
-        model=train_op.outputs["model"],
+        model=best_model_op.outputs["best_model"],
     )
 
 
